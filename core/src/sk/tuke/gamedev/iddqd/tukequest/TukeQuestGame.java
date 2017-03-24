8c533161b65c26aa2fa7fa556f9a064d51073581
scsc <scscgit@users.noreply.github.com>
2017-03-24 11:01:02 +0100

    Removed Tiled support and used FitViewport camera strategy.
    This fixed the wrong scaling problem, though graphics is still not okay.
    Temporarily trebled the example actor.
    Improved the order of rendering, physics will be drawn after bodies.

--- core/src/sk/tuke/gamedev/iddqd/tukequest/TukeQuestGame.java
+++ core/src/sk/tuke/gamedev/iddqd/tukequest/TukeQuestGame.java
@@ -5,32 +5,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import sk.tuke.gamedev.iddqd.tukequest.actors.Actor;
 import sk.tuke.gamedev.iddqd.tukequest.actors.BodyActor;
 import sk.tuke.gamedev.iddqd.tukequest.actors.ExampleActor;
-import sk.tuke.gamedev.iddqd.tukequest.actors.RectangleActor;
 import sk.tuke.gamedev.iddqd.tukequest.visual.Animation;
 
 public class TukeQuestGame extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
     private World world;
     private Box2DDebugRenderer debugRenderer;
+    private Viewport viewport;
 
     @Override
     public void create() {
@@ -39,42 +34,18 @@ public class TukeQuestGame extends ApplicationAdapter {
         // Initialize the World with a gravitation
         world = new World(new Vector2(0, -10), true);
 
-        // Load the map, set the unit scale to 1/32 (1 unit == 32 pixels)
-        TiledMap map = new TmxMapLoader().load("gameart2d.com-platformer/platformer-test.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
+        // Physics debug renderer
         debugRenderer = new Box2DDebugRenderer();
 
-        // Create an orthographic camera, which shows us width and height of 20 units of the World
+        // Setup an orthographic camera to support a virtual screen size, which always maintains the same aspect ratio
         camera = new OrthographicCamera();
-        updateCamera();
+        viewport = new FitViewport(500, 500, camera);
 
-        // Create a corresponding Actor for every map object
-        for (MapObject object : map.getLayers().get("Object Layer 1").getObjects()) {
-            createMapObject(object);
-        }
-
-        // Create an example Actor in the World
-        new ExampleActor(
-            new Animation("badlogic.jpg", 0.5f), BodyDef.BodyType.DynamicBody, 0, 300, camera
-        ).addToWorld(world);
-    }
-
-    private void createMapObject(MapObject object) {
-        // At the moment, we only support rectangles
-        if (object instanceof RectangleMapObject) {
-            RectangleMapObject rectangleObject = (RectangleMapObject) object;
-            Rectangle rectangle = rectangleObject.getRectangle();
-
-            new RectangleActor(
-                Animation.INVISIBLE,
-                BodyDef.BodyType.StaticBody,
-                rectangle.getX(),
-                rectangle.getY(),
-                rectangle.getWidth(),
-                rectangle.getHeight()
-            ).addToWorld(this.world);
-        } else {
-            System.out.println("Warning: your map contains unexpected " + object.getClass().getName());
+        // Create example Actors in the World
+        for (int i = 0; i < 3; i++) {
+            new ExampleActor(
+                new Animation("badlogic.jpg", 0.5f), BodyDef.BodyType.DynamicBody, i * 150, 300, camera
+            ).addToWorld(world);
         }
     }
 
@@ -99,13 +70,6 @@ public class TukeQuestGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Draw a background map
-        renderer.setView(camera);
-        renderer.render();
-
-        // Debug
-        debugRenderer.render(world, camera.combined.cpy().scl(1 / 32f));
-
         // Draw the images of all Actors
         batch.begin();
         Array<Body> worldBodies = new Array<Body>(0);
@@ -115,6 +79,9 @@ public class TukeQuestGame extends ApplicationAdapter {
             actor.draw(batch);
         }
         batch.end();
+
+        // Debug
+        debugRenderer.render(world, camera.combined);
     }
 
     private void calculatePhysics() {
@@ -137,21 +104,15 @@ public class TukeQuestGame extends ApplicationAdapter {
         }
     }
 
-    private void updateCamera() {
-        camera.setToOrtho(false, 20, 20);
-        camera.update();
-    }
-
     // Screen resized event
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
-        updateCamera();
+        viewport.update(width, height);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        renderer.dispose();
     }
 }
