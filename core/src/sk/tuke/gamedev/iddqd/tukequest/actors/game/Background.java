1e0f9ae363cff31cb7688d4aa0c5a350d41ff944
scsc <scscgit@users.noreply.github.com>
2017-03-26 21:18:36 +0200

    Fixed symmetrical Animation scale constructor, used it in Background.

--- core/src/sk/tuke/gamedev/iddqd/tukequest/actors/game/Background.java
+++ core/src/sk/tuke/gamedev/iddqd/tukequest/actors/game/Background.java
@@ -1,5 +1,6 @@
 package sk.tuke.gamedev.iddqd.tukequest.actors.game;
 
+import sk.tuke.gamedev.iddqd.tukequest.TukeQuestGame;
 import sk.tuke.gamedev.iddqd.tukequest.actors.AbstractAnimatedActor;
 import sk.tuke.gamedev.iddqd.tukequest.visual.Animation;
 
@@ -8,7 +9,8 @@ import sk.tuke.gamedev.iddqd.tukequest.visual.Animation;
  */
 public class Background extends AbstractAnimatedActor implements RenderFirst {
 
-    public static final Animation ANIMATION = new Animation("background.jpg");
+    public static final Animation ANIMATION = new Animation(
+        "background.jpg", Animation.ScaleType.SCALE_WIDTH, TukeQuestGame.SCREEN_WIDTH);
 
     public Background(float y) {
         super(ANIMATION, 0, y);
