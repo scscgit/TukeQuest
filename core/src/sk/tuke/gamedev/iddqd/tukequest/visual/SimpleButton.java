34222b5a874d9c777a8819862e2a65ee75fdfd27
Jozef Hudacek <jozo5610@gmail.com>
2017-04-16 16:48:31 +0200

    Fixed menu button colors and sizes

--- core/src/sk/tuke/gamedev/iddqd/tukequest/visual/SimpleButton.java
+++ core/src/sk/tuke/gamedev/iddqd/tukequest/visual/SimpleButton.java
@@ -10,46 +10,17 @@ public class SimpleButton {
 
     private Texture texture;
     private TextureRegion region;
-
-    public Texture getTexture() {
-        return texture;
-    }
-
-    public void setTexture(Texture texture) {
-        this.texture = texture;
-    }
-
-    public TextureRegion getRegion() {
-        return region;
-    }
-
-    public void setRegion(TextureRegion region) {
-        this.region = region;
-    }
-
-    public TextureRegionDrawable getDrawable() {
-        return drawable;
-    }
-
-    public void setDrawable(TextureRegionDrawable drawable) {
-        this.drawable = drawable;
-    }
-
-    public ImageButton getButton() {
-        return button;
-    }
-
-    public void setButton(ImageButton button) {
-        this.button = button;
-    }
-
     private TextureRegionDrawable drawable;
     private ImageButton button;
 
     public SimpleButton(String filename) {
-        texture = new Texture(Gdx.files.internal(filename));
-        region = new TextureRegion(texture);
-        drawable = new TextureRegionDrawable(region);
-        button = new ImageButton(drawable);
+        this.texture = new Texture(Gdx.files.internal(filename));
+        this.region = new TextureRegion(texture);
+        this.drawable = new TextureRegionDrawable(region);
+        this.button = new ImageButton(drawable);
+    }
+
+    public ImageButton getButton() {
+        return button;
     }
 }
