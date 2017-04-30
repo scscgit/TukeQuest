1bf6b7637ef7dc43972d81afcde957ce317a330c
Jakub Ruzicka <jakub.ruzicka@student.tuke.sk>
2017-04-30 18:53:35 +0200

    changed binas animation

--- core/src/sk/tuke/gamedev/iddqd/tukequest/actors/game/teachers/Binas.java
+++ core/src/sk/tuke/gamedev/iddqd/tukequest/actors/game/teachers/Binas.java
@@ -2,19 +2,18 @@ package sk.tuke.gamedev.iddqd.tukequest.actors.game.teachers;
 
 import com.badlogic.gdx.audio.Music;
 import sk.tuke.gamedev.iddqd.tukequest.TukeQuestGame;
-import sk.tuke.gamedev.iddqd.tukequest.actors.game.player.Player;
 import sk.tuke.gamedev.iddqd.tukequest.screens.GameScreen;
+import sk.tuke.gamedev.iddqd.tukequest.visual.Animation;
 
 /**
  * Created by Ruza on 5.4.2017.
  */
 public class Binas extends AbstractTeacher {
-
     public Binas(GameScreen screen, float x, float y) {
         super(
             screen,
-            Player.ANIMATION_LEFT_WALK,
-            Player.ANIMATION_RIGHT_WALK,
+            new Animation("binas.png", 8, 4, 16, 25, 0.2f),
+            new Animation("binas.png", 8, 4, 0, 9, 0.2f),
             x,
             y,
             TukeQuestGame.manager.get("audio/sounds/binas1.mp3", Music.class),
